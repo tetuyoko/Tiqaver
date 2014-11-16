bbeef7ddd0944a816ef11cfda1e121f95e042ce5
tetuyoko <tyokoyama53@gmail.com>
2014-11-16 11:15:26 +0900

    Initial Commit

---
+++ Tiqaver/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tiqaver
+//
+//  Created by tetuyoko on 2014/11/16.
+//  Copyright (c) 2014年 tetuyoko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tiqaver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tiqaver
+//
+//  Created by tetuyoko on 2014/11/16.
+//  Copyright (c) 2014年 tetuyoko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
